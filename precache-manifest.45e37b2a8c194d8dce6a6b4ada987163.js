0f8e9ef3ab5d1f19f2a35fbc05b88c51c96aed3b
Adil Dev <adilbekmomunaliev@Adilbeks-MacBook-Pro.local>
2020-01-02 18:57:47 -0800

    Updates

---
+++ precache-manifest.45e37b2a8c194d8dce6a6b4ada987163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d80d1d51f9dc327c9d57f5b4af41161",
+    "url": "/e-commerce-front-end/index.html"
+  },
+  {
+    "revision": "8ccc4403b431a76d7bbc",
+    "url": "/e-commerce-front-end/static/css/main.526d1a9f.chunk.css"
+  },
+  {
+    "revision": "b8e9eeeda3ae22fcac8e",
+    "url": "/e-commerce-front-end/static/js/2.b05d0c5e.chunk.js"
+  },
+  {
+    "revision": "8ccc4403b431a76d7bbc",
+    "url": "/e-commerce-front-end/static/js/main.03a5e2ed.chunk.js"
+  },
+  {
+    "revision": "3fea831c986b79989442",
+    "url": "/e-commerce-front-end/static/js/runtime-main.7cc461d0.js"
+  },
+  {
+    "revision": "b09f21048dbffa60fb3851b87429861d",
+    "url": "/e-commerce-front-end/static/media/fashon.b09f2104.png"
+  },
+  {
+    "revision": "ef9596448ca51501e4c8645fed2e6588",
+    "url": "/e-commerce-front-end/static/media/person.ef959644.png"
+  }
+]);
